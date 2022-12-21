7870cfa3446f558921d6384af6a352ae2c95ea4e
JonasRegener <regener.jonas@gmail.com>
2022-12-21 21:42:06 +0100

    added CustomUserModel

---
+++ movieflix/user/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):    
+    email               = models.EmailField(max_length=100, unique=True)
+    username            = models.CharField(max_length=30, unique=True)
+    date_joined         = models.DateTimeField( auto_now_add=True) #verbose_name="date joined",
+    last_login          = models.DateTimeField(auto_now=True) #verbose_name="last login", 
+    is_admin            = models.BooleanField(default=True)
+    is_active           = models.BooleanField(default=True)
+    is_staff            = models.BooleanField(default=True)
+    is_superuser        = models.BooleanField(default=True)
+  
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS= ['username']
+
+
+
+"""   profile_image       = models.ImageField() # rnd bild generieren? oder aus einer auswahl auswählen lassen? """
+
+
---
+++ movieflix/user/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import CustomUser
+
+# Create your views here.
