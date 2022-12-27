d51e5aada3155a3ee9fefa0a8e8ba983c4900bac
JonasRegener <regener.jonas@gmail.com>
2022-12-27 12:32:33 +0100

    fixed url

--- movieflix/movieflix/urls.py
+++ movieflix/movieflix/urls.py
@@ -26,6 +26,7 @@ router.register(r'user', CustomUserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
