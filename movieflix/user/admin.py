4c497ec3eeb1129d8f1b0d3814870b7cca8bd21c
Stefan797 <stefanhubner97@web.de>
2022-12-28 13:44:37 +0100

    rework Costum User Options

--- movieflix/user/admin.py
+++ movieflix/user/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+from django.forms import TextInput, Textarea
 
 # Register your models here.
 
-admin.site.register(CustomUser)
+class UserAdminConfig(UserAdmin):
+    model = CustomUser
+    search_fields = ('email', 'user_name', 'first_name',)
+    list_filter = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
+    ordering = ('date_joined',)
+    list_display = ('email', 'user_name', 'first_name',
+                    'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'user_name', 'first_name',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')})
+    )
+   
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'user_name', 'first_name', 'password1', 'password2', 'is_active', 'is_staff')}
+         ),
+    )
+
+
+admin.site.register(CustomUser, UserAdminConfig)
--- movieflix/user/models.py
+++ movieflix/user/models.py
@@ -1,19 +1,44 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-class CustomUser(AbstractUser):    
-    date_joined         = models.DateTimeField(auto_now_add=True) #verbose_name="date joined",
-    last_login          = models.DateTimeField(auto_now=True) #verbose_name="last login", 
-    is_admin            = models.BooleanField(default=True)
-    is_active           = models.BooleanField(default=True)
-    is_staff            = models.BooleanField(default=True)
-    is_superuser        = models.BooleanField(default=True)
-    first_name          = models.CharField(max_length=50)
-    last_name           = models.CharField(max_length=50)
-    password            = models.CharField(max_length=999)
-    email               = models.CharField(max_length=300, unique=True)
-    username            = models.CharField(max_length=300, unique=True)
+class CustomAccountManager(BaseUserManager):
 
+   def create_user(self, email, user_name, first_name, password, **other_fields):
+
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name,
+                          first_name=first_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):    
+        date_joined         = models.DateTimeField(default=timezone.now) #verbose_name="date joined",
+        is_admin            = models.BooleanField(default=True)
+        is_active           = models.BooleanField(default=True)
+        is_staff            = models.BooleanField(default=True)
+        is_superuser        = models.BooleanField(default=True)
+        first_name          = models.CharField(max_length=50)
+        last_name           = models.CharField(max_length=50)
+        password            = models.CharField(max_length=999)
+        email               = models.EmailField(_('email address'), unique=True)
+        user_name           = models.CharField(max_length=300, unique=True)
+
+        objects = CustomAccountManager()
+
+        USERNAME_FIELD = 'email'
+        REQUIRED_FIELDS = ['user_name', 'first_name']
+
+        def __str__(self):
+            return self.user_name
+
+
+#last_login = models.DateTimeField(default=timezone.now) #verbose_name="last login", 
 
  #   USERNAME_FIELD = 'email'
  #   REQUIRED_FIELDS= ['username']
