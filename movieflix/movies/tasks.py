d7c3122606391a9a0aca30aa6a79265efc6d067d
Stefan797 <stefanhubner97@web.de>
2022-12-28 17:30:55 +0100

    refactoring convert480p function at tasks

--- movieflix/movies/tasks.py
+++ movieflix/movies/tasks.py
@@ -8,17 +8,13 @@ def convert_720p(source):
     subprocess.run(cmd)
 
 def convert480p(source):
-    path = os.path.join(settings.MEDIA_ROOT, 'movie' )
     ext = source.rsplit('\x5c', 1)[0]
-    final_path = ext + '\x5c'
-    destination_path = ext + '_480p.mp4'
-    new_path = source.split('\x5c')[-1]
-    new_path2 = new_path.split('.')[0] + '_480p.mp4'
-    source_path = final_path + ext
-    
-    test = r"C:\dev\backend\Neuer Ordner\Movieflix-Backend\movieflix\media\movie\Ozean135658.mp4"
-    test2 = r"C:\dev\backend\Neuer Ordner\Movieflix-Backend\movieflix\media\movie\Ozean135658_480p.mp4"
-    print(test)
-    print(test2)
-    cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(test, test2)    
+    path = ext + '\x5c'
+    input_ending = source.split('\x5c')[-1]
+    output_ending = input_ending.split('.')[0] + '_480p.mp4'
+
+    input_path = path + input_ending
+    output_path = path + output_ending
+
+    cmd = 'ffmpeg -i "{}" -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 "{}"'.format(input_path, output_path) 
     subprocess.run(cmd, capture_output=True)
